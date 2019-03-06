bfce3711869d5006af54e62f9d70caee3046950e
CORP\dambers <daimen.ambers@capgemini.com>
2019-03-06 10:45:52 -0500

    Changing url so that the addValue function will work

--- scanToThingworx.py
+++ scanToThingworx.py
@@ -5,13 +5,13 @@ try:
 except: # For Python 3
     import urllib.parse as urlparse
     from urllib.parse import urlencode
-    
+
 import requests
 import json
 
 #scanner for rasperrypi to load onto thingworx
 data = raw_input("Please scan an item: ")
-    
+
 def thingSearch(data):
     print("Item number: " + data)
     url = "https://pp-1901291615ip.devportal.ptc.io/Thingworx/Things/" + data
@@ -19,7 +19,7 @@ def thingSearch(data):
               ,"appKey": "f1a0b8da-0c90-4a51-a26e-584d3bf9e6e8"
 	      ,"Accept": "application/json"
               }
-    
+
     response = requests.get(url, headers=header)
     print(response.status_code)
     global code
@@ -35,7 +35,7 @@ def enableThing(data):
               }
     response = requests.post(url, headers=header)
     print(response.status_code)
-    
+
 def restartThing(data):
     url = "https://pp-1901291615ip.devportal.ptc.io/Thingworx/Things/"+data+"/Services/RestartThing"
     header = {"Content-Type": "application/json"
@@ -43,7 +43,7 @@ def restartThing(data):
               }
     response = requests.post(url, headers=header)
     print(response.status_code)
-    
+
 def addThing(data):
     dataName = raw_input("Please give scanned item a display name: ")
     url = "https://pp-1901291615ip.devportal.ptc.io/Thingworx/Resources/EntityServices/Services/CreateThing"
@@ -51,7 +51,7 @@ def addThing(data):
         "Content-Type": "application/json"
         ,"appKey": "f1a0b8da-0c90-4a51-a26e-584d3bf9e6e8"
         }
-    
+
     parameters = {
         "name": data
         ,"thingTemplateName": "GenericThing"
@@ -61,7 +61,7 @@ def addThing(data):
     print(response.status_code)
     print(response.content)
     print(json.dumps(parameters))
-    
+
 def addProperties(data):
     url = "https://pp-1901291615ip.devportal.ptc.io/Thingworx/Things/"+data+"/Services/AddPropertyDefinition"
     header = {"Content-Type": "application/json"
@@ -73,30 +73,30 @@ def addProperties(data):
     description = raw_input("Please give a descrption of the property: ")
     parameters = {
         "name": propertyName
-        ,"type": ptype 
+        ,"type": ptype
         ,"description": description
         }
     response = requests.post(url, headers=header, params=parameters)
     print(response.status_code)
     print(response.content)
     print(json.dumps(parameters))
-    
+
 def addValues(data):
     propertyName = raw_input("Which property would you like to change? ")
-    url = "https://pp-1901291615ip.devportal.ptc.io/Thingworx/Things/"+data+"/Properties/"+propertyName
+    url = "https://pp-1901291615ip.devportal.ptc.io/Thingworx/Things/"+data+"/Properties/*"
     header = {"Content-Type": "application/json"
               ,"appKey": "f1a0b8da-0c90-4a51-a26e-584d3bf9e6e8"
               }
     value = raw_input("Please give property a value: ")
     parameters = {
-        propertyName : value
+        str(propertyName) : str(value),
         }
     response = requests.put(url, headers=header, params=parameters)
     print(response.status_code)
     print(response.content)
     print(json.dumps(parameters))
-    
-try:   
+
+try:
     thingSearch(data)
     if code != 200:
         addThing(data)
@@ -113,6 +113,6 @@ try:
             addValues(data)
         else:
             print("Exiting Program")
-        
+
 except KeyboardInterrupt:
     print("Ctrl+C Programing Exiting")
